b3f27c5d4b9c9c167fd43d2b46a1f022ebd16ace
Poulycroc <barti_ma@babar.local>
2016-01-29 20:17:56 +0100

    Genrepremière version truc
    voila le début que j’ai tapé
    que j’ai presque fini avec un K stp

---
+++ include/footer.php
@@ -0,0 +1,53 @@
+<div class="contact warp _pad _clear _f_w">
+	<form class="contact_form" method="post" action="">
+		<label for="nom">Nom</label>
+		<input  type="text" placeholder="Nom" name="nom" tabindex="1" />
+
+		<label for="prenom">Prénom</label>
+		<input  type="text" placeholder="Prénom" name="prenom" tabindex="1" />
+
+		<label for="message">Message</label>
+		<textarea name="message" placeholder="Indiquez ici votre message" tabindex="4" cols="30" row="8"></textarea>
+
+		<div class="__col --6 --fl --br">
+			<label for="ville">Ville</label>
+			<input  type="text" placeholder="Nom" name="ville" tabindex="1" />
+		</div>
+		<div class="__col --6 --fr --bl">
+			<label for="postal">Code Poste</label>
+			<input  type="text" placeholder="Code postal" name="postal" tabindex="1" />
+		</div>
+
+		<label for="rue">Rue</label>
+		<input  type="text" placeholder="Rue" name="rue" tabindex="1" />
+			
+		<label for="telephone">Téléphone</label>
+		<input  type="text" placeholder="Téléphone" name="telephone" tabindex="1" />
+
+		<div class="keyline"></div>
+		
+		<div class="__cais --submit _trs">
+			<p class="_trs">Envoyer</p>
+		<input type="submit" name="envoi" value="Envoyer" />
+		</div>
+		<div class="info_submit">
+			<p>Ou envoyer directement un <a href="mailto: gmail@gmail.com">mail</a>.</p>
+		</div>	
+	</form>
+</div><!-- contact -->
+<footer class='_f_w _pad _clear'>
+	<div class="warp">
+		<ul class="info">
+		    <li>tel</li>
+		    <li>mail</li>
+		    <li>adresse</li>
+		</ul>
+		<ul class="sponsors">
+		    <li></li>
+		    <li></li>
+		    <li></li>
+		    <li></li>
+		</ul>
+		<small>réalisé par Maxime Bartier et Daniel Vos</small>
+	</div><!-- warp -->
+</footer>
