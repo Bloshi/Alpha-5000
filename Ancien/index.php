8f9bd3e31fe26e17730fa979edf65711cf47bebc
Bloshi <danielgtavc@hotmail.com>
2016-03-01 18:13:20 +0100

    tada

---
+++ Ancien/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title>New Project</title>
+	<meta charset="utf-8" />
+	
+	<?php include("include/meta.php") ?>
+
+	<link rel="icon" type="image/png" href="favicon.png" />
+	<style>
+		@import url(assets/css/main.css);
+	</style>
+	<script src="assets/js/jquery-1.11.1.min.js"></script>
+</head>
+<body>
+
+	<header class='_f_w _clear'>
+		<div class="logo">
+			<img src="assets/img/Logo Alpha5000.png" alt="logo Alpha5000 Site Web" />
+		</div>
+		<cite>
+			
+		</cite>
+	</header>
+	<nav class="main _f_w">lol
+		
+	</nav>
+	<main class='_f_w _pad _clear'>
+		<article class="warp">
+			<div class="__col --fl --8">
+				<p>Depuis maintenant 30 ans, l'association développe des formations d'alphabétisation, de remise à niveau</p>
+				<p>Notre site se veut un aperçu général de notre action en vue de multiplier les contacts avec des partenaires potentiels, qu'ils soient opérateurs de formation, pouvoirs locaux, CPAS, EFT, etc.</p>
+				<p>A travers toutes nos actions, nous voulons promouvoir le changement citoyen, une culture promouvant la solidarité et la justice dans la société, pour le bien être de tous.</p>
+				<p>Notre méthode préconise la co-construction des savoirs, savoirs faire, savoirs être entre formateurs et participants dans une démarche collective d’éducation permanente et d’interculturalité.</p>
+			</div>
+			<div class="__col --fr --4">
+				<!-- super img -->
+			</div>
+		</article>
+		<div class="row warp _clear actualite">
+			<h2>Actualité</h2>
+			<div class="slide">
+				
+			</div>
+		</div>
+		<div class="row dl_fichier warp">
+			
+		</div>
+
+	</main>
+
+	<?php include('include/footer.php'); ?>
+
+<!-- SCRIPTS -->	
+<script src="assets/js/main.js"></script>
+</body>
+</html>
